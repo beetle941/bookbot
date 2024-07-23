0057065495349fda7219f4d8b2794e4f96e7f4eb
beetle941 <adrianmurillo892005@gmail.com>
2024-07-23 13:33:21 -0500

    4: updating code to sort letters

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ def main():
         text = f.read()
         words = text.split()
         number_of_words = len(words)
+        
         Letters = {}
         for letter in text:
             lowered = letter.lower()
-            if lowered in Letters:
-                Letters[lowered] += 1
-            else:
-                Letters[lowered] = 1
-        print(Letters)
+            if lowered.isalpha():    
+                if lowered in Letters:
+                    Letters[lowered] += 1
+                
+                else:
+                    Letters[lowered] = 1
+        sorted_letters = sorted(Letters.items(), key=lambda item: item[1], reverse=True)
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{number_of_words} words found in the document")
+        
 
 
 
