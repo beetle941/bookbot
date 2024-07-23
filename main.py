0e9acfe3241654d5840dd0f277eca57cd027876a
beetle941 <adrianmurillo892005@gmail.com>
2024-07-23 00:14:48 -0500

    3: updating main.py to sort all charicters

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-with open('books/frankenstein.txt') as file:
-    file_contents = file.read()
-    print(file_contents)
+def main():
+    with open('books/frankenstein.txt') as f:
+        text = f.read()
+        words = text.split()
+        number_of_words = len(words)
+        Letters = {}
+        for letter in text:
+            lowered = letter.lower()
+            if lowered in Letters:
+                Letters[lowered] += 1
+            else:
+                Letters[lowered] = 1
+        print(Letters)
+
+
+
+main()
